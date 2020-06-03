9c7f2e255e9d5cd9d39a97d698bd425875f847a2
James McDonald <james_mcd98@hotmail.com>
2020-06-02 20:51:17 -0400

    Added random username creation from file

--- services/usernameService.js
+++ services/usernameService.js
@@ -1,5 +1,10 @@
+const fs = require('fs')
+const usernameData = require('../assets/usernameData.json')
+
 module.exports = {
-    getUserName: function(){
-        return "username "+Math.random();
+    getUserName: function () {
+        const index = Math.floor(Math.random() * 10000) % usernameData["names"].length
+        return "user " + usernameData["names"][index]
+
     }
 }
